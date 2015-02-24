29817e53a700f1872206428bfc116609d25acfb3
Bình Nguyên <crziter@gmail.com>
2015-02-24 13:13:51 +0000

    Hello world kernel (real mode)

--- kernel/start.asm
+++ kernel/start.asm
@@ -1,9 +1,21 @@
 [org 0x9000]
 
-mov al, 'K'
+mov si, kernel_hello
+
+print:
+lodsb
+cmp al, 0
+je .out
+
 mov ah, 0x0E
 xor bx, bx
 int 0x10
+jmp print
 
+.out:
 cli
 hlt
+
+
+kernel_hello:
+db 'Hello from KERNEL \m/', 0

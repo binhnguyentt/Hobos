103deb4a8bc6a5851793805166c77f5408bf6b62
Bình Nguyên <crziter@gmail.com>
2015-02-28 22:13:33 +0000

    Thêm các hằng số lưu vị trí các khối

--- bootloader/memory.asm
+++ bootloader/memory.asm
@@ -5,5 +5,8 @@
 ; Memory map
 ; 0x500: Global variables
 
-ROOT_DIR_LBA	equ 0x500	; 8 bytes
+ROOT_DIR_LBA	equ 0x500			; 8 bytes
+DRIVE_NUMBER	equ (0x500 + 0x08)	; 1 byte
 
+STAGE2_OFFSET	equ 0x600
+ROOTDIR_OFFSET	equ 0x8000

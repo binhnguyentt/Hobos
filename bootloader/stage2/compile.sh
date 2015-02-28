74d89769614267d1d868c467cbdb6fdcee40bd69
Bình Nguyên <crziter@gmail.com>
2015-02-28 22:35:42 +0000

    Thêm trình xử lí

--- bootloader/stage2/compile.sh
+++ bootloader/stage2/compile.sh
@@ -1,3 +1,6 @@
-gcc -c -g -Os -m32 -march=i686 -ffreestanding -Wall -Werror -I. -o stage2.o stage2.c
+#!/bin/bash
+
+gcc -c -g -Os -m32 -march=i686 -ffreestanding -Wall -Werror -I. -o 
+stage2.o stage2.c
 ld -static -Tlinker.ld -nostdlib --nmagic -o stage2.elf stage2.o -melf_i386
 objcopy -O binary stage2.elf stage2

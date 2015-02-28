457b4d64067f88a7ba450ea87d571d4bf9c42024
Bình Nguyên <crziter@gmail.com>
2015-02-28 19:16:12 +0000

    Bắt đầu viết stage2

---
+++ bootloader/stage2/compile.sh
@@ -0,0 +1,3 @@
+gcc -c -g -Os -m32 -march=i686 -ffreestanding -Wall -Werror -I. -o stage2.o stage2.c
+ld -static -Tlinker.ld -nostdlib --nmagic -o stage2.elf stage2.o -melf_i386
+objcopy -O binary stage2.elf stage2

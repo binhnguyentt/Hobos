c6a8d8cf5f2f4d352bfad92f8cef6df5dfc8dc4e
Bình Nguyên <crziter@gmail.com>
2015-02-28 22:29:32 +0000

    Clean comment

--- bootloader/memory.asm
+++ bootloader/memory.asm
@@ -3,10 +3,17 @@
 
 
 ; Memory map
-; 0x500: Global variables
+; 0x500: Uninitialize variables
 
-ROOT_DIR_LBA	equ 0x500			; 8 bytes
+; Store LBA of FAT32 root directory
+ROOT_DIR_LBA	equ 0x500			; 8 bytes 
+
+; Drive number: after POST, BIOS stores drive number
+; in dl register, we take it and put here for later use (read operation)
 DRIVE_NUMBER	equ (0x500 + 0x08)	; 1 byte
 
+; Stage2 offset
 STAGE2_OFFSET	equ 0x600
+
+; We temporary load FAT32 root directory to this address
 ROOTDIR_OFFSET	equ 0x8000

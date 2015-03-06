f58a5eea3c13235efaaa67f8f095a1f9754dc494
Bình Nguyên <crziter@gmail.com>
2015-03-06 17:19:20 +0700

    Dùng biến BSS_ADDR thay vì fixed number

--- bootloader/memory.asm
+++ bootloader/memory.asm
@@ -5,12 +5,14 @@
 ; Memory map
 ; 0x500: Uninitialize variables
 
+BSS_ADDR		equ 0x500
+
 ; Store LBA of FAT32 root directory
-ROOT_DIR_LBA	equ 0x500			; 8 bytes 
+ROOT_DIR_LBA	equ BSS_ADDR			; 8 bytes 
 
 ; Drive number: after POST, BIOS stores drive number
 ; in dl register, we take it and put here for later use (read operation)
-DRIVE_NUMBER	equ (0x500 + 0x08)	; 1 byte
+DRIVE_NUMBER	equ (BSS_ADDR + 0x08)	; 1 byte
 
 ; Stage2 offset
 STAGE2_OFFSET	equ 0x600
